4b784d1bb93369d22cacf26ac6a762f8ee1ec088
doikimihiro <doikimihiro@gmail.com>
2018-09-24 19:15:38 +0900

    √nの素因数分解を追加

--- prime_factrorize.cpp
+++ prime_factrorize.cpp
@@ -30,5 +30,16 @@ map<ll, int> prime_factrorize(ll n, vector<int> const & primes) {
 	if (n != 1) result[n] += 1;
 	return result;
 }
+map<int, int> prime_factor(int n) {
+	map<int, int>res;
+	for (int i = 2; i*i <= n; i++) {
+		while (n%i == 0) {
+			++res[i];
+			n /= i;
+		}
+	}
+	if (n != 1)res[n] = 1;
+	return res;
+}
 signed main() {
 }
